8f454c22b3e6ab360c23753073aeb6f3ca92bd72
nikhil <r.nikhil1999@gmail.com>
2020-03-17 23:42:31 +0530

    Sarcasm_Detection

---
+++ Sarcasm_detection.py
@@ -0,0 +1,51 @@
+import json
+
+with open("Sarcasm.json", "r") as f:
+    datastore = json.load(f)
+
+sentences = []
+labels = []
+urls = []
+
+for item in datastore:
+    sentences.append(item['headline'])
+    labels.append(item['is_sarcastic'])
+    urls.append(item['article_link'])
+
+import tensorflow as tf
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+
+training_size = 20000
+training_sentences = sentences[0:training_size]
+testing_sentences = sentences[training_size:]
+
+training_labels = labels[0:training_size]
+testing_labels = labels[training_size:]
+
+tokenizer = Tokenizer(oov_token="<OOV>")
+tokenizer.fit_on_texts(training_sentences)
+
+word_index = tokenizer.word_index
+
+training_sequences = tokenizer.texts_to_sequences(training_sentences)
+training_paded = pad_sequences(training_sequences)
+
+test_sequences = tokenizer.texts_to_sequences(testing_sentences)
+testing_padding = pad_sequences(testing_sentences)
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(10000, 16, input_length=100),
+    tf.keras.layers.GlobalAveragePooling1D(),
+    tf.keras.layers.Dense(24, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+epochs = 30
+history = model.fit(training_paded, training_labels, epochs=epochs, validation_data=(testing_padding, testing_labels), verbose=2)
+
+sentence = ["granny starting to fear spiders in the garden might be real", "game of thrones season finale showing this sunday night"]
+sequences = tokenizer.texts_to_sequences(sentence)
+padded = pad_sequences(sequences)
+print(model.predict(padded))
